d4173ebd56ff6b75888c0f2803ecf4c259c7ca01
ceridwenroberts <ceridwen.roberts@gmail.com>
2024-12-19 14:19:28 +0100

    Add formatting to test files - Prettier

--- src/__test__/NewGameButton.test.tsx
+++ src/__test__/NewGameButton.test.tsx
@@ -2,11 +2,11 @@ import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import NewGameButton from "../components/NewGameBtn";
 
 test("Check that New Game button renders correctly", () => {
-    const mockFunction = () => {
-        return
-    }
-    render(<NewGameButton newRound={mockFunction}/>)
-    const button = screen.getByRole("button")
+  const mockFunction = () => {
+    return;
+  };
+  render(<NewGameButton newRound={mockFunction} />);
+  const button = screen.getByRole("button");
 
-    expect(button).toBeInTheDocument()
-})
+  expect(button).toBeInTheDocument();
+});
