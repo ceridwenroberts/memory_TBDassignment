874ee9faf2bb02ee3f2c771c55c42b0423a5276d
ceridwenroberts <ceridwen.roberts@gmail.com>
2024-12-20 03:07:07 +0100

    Import Rules to Footer and add tempate styling

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,18 @@
+import Rules from "../Rules";
 const Footer = () => {
   return (
-    <div role="contentinfo">
-      <div data-testid="copyright">
+    <div
+      className="mt-auto grid grid-cols-2 gap-x-4 p-4 bg-orange-800"
+      role="contentinfo"
+    >
+      <div
+        className="flex align-items-start place-self-end text-xs lg:text-base"
+        data-testid="copyright"
+      >
         <p>© Darius Kaya</p>
+      </div>
+      <div>
+        <Rules />
       </div>
     </div>
   );
--- src/components/Rules/index.tsx
+++ src/components/Rules/index.tsx
@@ -1,10 +1,14 @@
 const Rules = () => {
   return (
     <>
-      <div>
-              <h4>Rules</h4>
-              <p data-testid="rules-text">Click a card to reveal an image. Then click another card to reveal that image. Your job is to remember the images and click two of the same cards. Good luck beating the highscore!</p>
-              <p data-testid="good-luck">Good Luck!</p>
+      <div className="text-xs lg:text-base">
+        <h4 className="underline underline-offset-2 ">Rules</h4>
+        <p data-testid="rules-text">
+          Click a card to reveal an image. Then click another card to reveal
+          that image. Your job is to remember the images and click two of the
+          same cards. Good luck beating the highscore!
+        </p>
+        <p data-testid="good-luck">Good Luck!</p>
       </div>
     </>
   );
