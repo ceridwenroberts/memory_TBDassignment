9d90995e8b28f95f9f0b6d404c0730b3fa418138
ceridwenroberts <ceridwen.roberts@gmail.com>
2024-12-20 01:02:37 +0100

    move Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import Header from "../components/Header"
+import Header from "../components/Header";
 export default function Home() {
-    return (
-        <main>
-<Header />
-        </main>
-    );
+  return (
+    <>
+      <Header />
+      <main></main>
+    </>
+  );
 }
