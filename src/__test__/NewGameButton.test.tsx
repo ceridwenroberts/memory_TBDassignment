3fdf7f85517e1981cc9fdf4bdc0a87f7df626689
ceridwenroberts <ceridwen.roberts@gmail.com>
2024-12-20 03:24:58 +0100

    Add import of NewGameButton

--- src/__test__/NewGameButton.test.tsx
+++ src/__test__/NewGameButton.test.tsx
@@ -1,5 +1,5 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import NewGameButton from "../components/NewGameBtn";
+import NewGameButton from "../components/NewGameButton";
 
 test("Check that New Game button renders correctly", () => {
   const mockFunction = () => {
