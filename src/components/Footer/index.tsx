6cc9a8ed55f154fb758329a3c734ec70161761d3
ceridwenroberts <ceridwen.roberts@gmail.com>
2024-12-20 01:25:08 +0100

    Add Footer component logic. Footer tests passed.

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div role="contentinfo">
+      <div data-testid="copyright">
+        <p>© Darius Kaya</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
