3d3f7b7876fb8fad26b90291efbd05a86e58b943
ceridwenroberts <ceridwen.roberts@gmail.com>
2024-12-20 01:54:14 +0100

    Add Rules component logic. Tests passed.

--- src/components/Rules/index.tsx
+++ src/components/Rules/index.tsx
@@ -1,6 +1,12 @@
 const Rules = () => {
-    return (
-        <></>
-    )
-}
-export default Rules
+  return (
+    <>
+      <div>
+              <h4>Rules</h4>
+              <p data-testid="rules-text">Click a card to reveal an image. Then click another card to reveal that image. Your job is to remember the images and click two of the same cards. Good luck beating the highscore!</p>
+              <p data-testid="good-luck">Good Luck!</p>
+      </div>
+    </>
+  );
+};
+export default Rules;
