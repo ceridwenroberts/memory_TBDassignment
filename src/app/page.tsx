041e9fdf9de0dadebacf0d414c6197b21cf8e83f
ceridwenroberts <ceridwen.roberts@gmail.com>
2024-12-27 16:53:45 +0100

    add game board to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import GameBoard from "@/components/GameBoard";
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="m-auto">Main content</main>
+      <main className="m-auto">Main content
+        <GameBoard />
+      </main>
       <Footer />
     </>
   );
