e9db6f14238dcdbc7ad5d3f03ff5b6fcce5ba278
ceridwenroberts <ceridwen.roberts@gmail.com>
2025-01-06 23:04:39 +0100

    add conditional name render and end of game handler props

--- src/components/ScoreBoard/index.tsx
+++ src/components/ScoreBoard/index.tsx
@@ -3,19 +3,20 @@ type ScoreBoardProps = {
   moves: number;
   highscore: number;
   name: string;
+  handleNewGame: () => void;
 };
 
-const ScoreBoard = ({ moves, highscore, name }: ScoreBoardProps) => {
+const ScoreBoard = ({ moves, highscore, name, handleNewGame }: ScoreBoardProps) => {
   return (
     <div className="flex w-full bg-slate-300">
       <div className="flex-1">
         <p>Moves:</p>
         <p data-testid="moves">{moves}</p>
       </div>
-      <NewGameButton data-testid="new-game-btn" />
+      <NewGameButton data-testid="new-game-btn" handleNewGame={ handleNewGame }/>
       <div className="flex-1">
         <p>High Score:</p>
-        <p data-testod="highscore-name">{`${name}:`}</p>
+        { name && <p data-testod="highscore-name">{ `${name}:` }</p> }
         <p data-testid="highscore">{highscore}</p>
       </div>
     </div>
