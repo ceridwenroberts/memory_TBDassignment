ea548290e983ddbbb08650c794142054a66cd0ec
ceridwenroberts <ceridwen.roberts@gmail.com>
2025-01-07 15:57:58 +0100

    update game context

--- src/contexts/GameContext.tsx
+++ src/contexts/GameContext.tsx
@@ -9,12 +9,25 @@ import {
 
 import { type GameState } from "@/lib/types";
 import cardsData from "@/data/cardsData.json";
-import { type CardType } from "@/lib/types";
+import { type CardType, type HighscoreObjType } from "@/lib/types";
 import { cardShuffler } from "@/util/cardShuffler";
+
 type GameContextType = {
   gameState: GameState;
   setGameState: Dispatch<SetStateAction<GameState>>;
+  cardsData: CardType[];
   startNewGame: () => void;
+
+  moves: number;
+  setMoves: Dispatch<SetStateAction<number>>;
+  highscoreObj: HighscoreObjType;
+  setHighscoreObj: Dispatch<SetStateAction<HighscoreObjType>>;
+  showForm: boolean;
+  setShowForm: Dispatch<SetStateAction<boolean>>;
+  flippedCards: number[];
+  setFlippedCards: Dispatch<SetStateAction<number[]>>;
+  pairedCardsArray: string[];
+  setPairedCardsArray: Dispatch<SetStateAction<string[]>>;
 };
 
 interface GameProviderProps {
@@ -25,11 +38,21 @@ interface GameProviderProps {
 const GameContext = createContext<GameContextType | null>(null);
 
 export const GameProvider = ({ children, cardsData }: GameProviderProps) => {
+  const [moves, setMoves] = useState<number>(0);
+  const [showForm, setShowForm] = useState<boolean>(false);
+  const [flippedCards, setFlippedCards] = useState<number[]>([]);
+  const [pairedCardsArray, setPairedCardsArray] = useState<string[]>([]);
+  const [highscoreObj, setHighscoreObj] = useState<HighscoreObjType>({
+    highscore: 0,
+    name: "",
+  });
+
   const newGameState: GameState = {
     gameCards: cardShuffler(cardsData),
     flippedCards: [],
     pairedCardsArray: [],
     moves: 0,
+    showForm: false,
   };
 
   const [gameState, setGameState] = useState<GameState>(newGameState);
@@ -39,13 +62,30 @@ export const GameProvider = ({ children, cardsData }: GameProviderProps) => {
   };
 
   return (
-    <GameContext.Provider value={{ gameState, setGameState, startNewGame }}>
+    <GameContext.Provider
+      value={{
+        gameState,
+        setGameState,
+        moves,
+        setMoves,
+        highscoreObj,
+        setHighscoreObj,
+        showForm,
+        flippedCards,
+        setFlippedCards,
+        pairedCardsArray,
+        setPairedCardsArray,
+        setShowForm,
+        startNewGame,
+        cardsData,
+      }}
+    >
       {children}
     </GameContext.Provider>
   );
 };
 
-export const useGame = (): GameContextType => {
+export const useGameContext = (): GameContextType => {
   const context = useContext(GameContext);
   if (!context) {
     throw new Error(
