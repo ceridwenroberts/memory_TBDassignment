6bf84af64c85d865be4eb01bb00a6280aec79dab
ceridwenroberts <ceridwen.roberts@gmail.com>
2025-01-08 22:47:50 +0100

    add functionality and styling to New Game button

--- src/components/NewGameButton/index.tsx
+++ src/components/NewGameButton/index.tsx
@@ -1,5 +1,21 @@
+import { useGameContext } from "@/contexts/GameContext";
+import { useState } from "react";
+
 const NewGameButton = () => {
-  return <button className="flex-1">New Game</button>;
+  const { pairedCards, setNewGame, gameEnd, setGameEnd } = useGameContext();
+  const [gameStarted, setGameStarted] = useState<boolean>(false);
+  pairedCards.length === 6 && setGameEnd(true);
+  return (
+    <button
+      className={`border border-orange-800 rounded-md py-2 px-4 text-center text-orange-800 text-sm hover:shadow-lg ${
+        gameStarted ? "bg-orange-800 bg-opacity-25 text-opacity-100" : ""
+      }${gameEnd && "bg-orange-800 text-orange-50 "}`}
+      onClick={() => setNewGame(true)}
+      data-testid="new-game-btn"
+    >
+      New Game
+    </button>
+  );
 };
 
 export default NewGameButton;
