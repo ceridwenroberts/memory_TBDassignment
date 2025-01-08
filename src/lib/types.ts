7185753de20904cc08c79c12b412df9f6ae52cea
ceridwenroberts <ceridwen.roberts@gmail.com>
2025-01-08 22:33:07 +0100

    update types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,13 +1,50 @@
-export  type HighscoreObjType = {
-    name: string,
-    highscore: number
-}
-  
+import { Dispatch, SetStateAction } from "react";
+export type HighscoreObjType = {
+  name: string;
+  highscore: number;
+};
 export type CardType = {
-    id: number;
-    name: string;
-    imgSrc: string;
-    reverse: string;
-    flipped?: boolean;
-    gameId?: number;
+  id: number;
+  name: string;
+  imgSrc: string;
+  reverse: string;
+  flipped?: boolean;
+  gameId?: number;
+};
+
+export interface INewHighscore {
+  name: string;
+  highscore: number;
 }
+
+export type GameStateType = {
+  gameCards: CardType[];
+  flippedCards: number[];
+  pairedCards: string[];
+  moves: number;
+  showForm: boolean;
+  highscoreObj: HighscoreObjType;
+};
+
+export type GameStateActions = {
+  setGameCards: Dispatch<SetStateAction<CardType[]>>;
+  setFlippedCards: Dispatch<SetStateAction<number[]>>;
+  setPairedCards: Dispatch<SetStateAction<string[]>>;
+  setMoves: Dispatch<SetStateAction<number>>;
+  setShowForm: Dispatch<SetStateAction<boolean>>;
+  setHighscoreObj: Dispatch<SetStateAction<HighscoreObjType>>;
+};
+
+export type NewGameProps = {
+  setFlippedCards: Dispatch<SetStateAction<number[]>>;
+  setPairedCards: Dispatch<SetStateAction<string[]>>;
+  setMoves: Dispatch<SetStateAction<number>>;
+  setShowForm: Dispatch<SetStateAction<boolean>>;
+  setHighscoreObj: Dispatch<SetStateAction<HighscoreObjType>>;
+  setSolved: Dispatch<SetStateAction<boolean>>;
+  setNewGame: Dispatch<SetStateAction<boolean>>;
+  cards: CardType[];
+  cardShuffler: () => CardType[],
+  setGameCards: Dispatch<SetStateAction<CardType[]>>
+  
+};
