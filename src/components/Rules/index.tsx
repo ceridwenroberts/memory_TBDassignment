a5896cc705554a62bfae7ee848e8e853f296215c
ceridwenroberts <ceridwen.roberts@gmail.com>
2025-01-08 22:45:12 +0100

    minor styling to Rules

--- src/components/Rules/index.tsx
+++ src/components/Rules/index.tsx
@@ -2,7 +2,7 @@ const Rules = () => {
   return (
     <>
       <div className="text-xs lg:text-base">
-        <h4 className="underline underline-offset-2 ">Rules</h4>
+        <h4 className="underline-offset-2 font-medium">Rules</h4>
         <p data-testid="rules-text">
           Click a card to reveal an image. Then click another card to reveal
           that image. Your job is to remember the images and click two of the
