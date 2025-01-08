c495bf15d2f9e45181d0f1db87c540436c863181
ceridwenroberts <ceridwen.roberts@gmail.com>
2025-01-08 22:35:25 +0100

    update meta export

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
+"use client"
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GameProvider } from "@/contexts/GameContext"
+import cardsData from "@/data/cardsData.json"
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+const metadata: Metadata = {
   title: "Memorista",
   description: "The ultimate mfkin memory game",
 };
@@ -16,10 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`flex min-h-screen ${inter.className}`}>
+      <body className={ `flex min-h-screen ${inter.className}` }>
+            {/* <GameProvider cardsData={cardsData}> */}
         <section className="flex w-full flex-col justify-between min-h-full">
           {children}
-        </section>
+          </section>
+          {/* </GameProvider> */}
       </body>
     </html>
   );
