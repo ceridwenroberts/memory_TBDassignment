aed97a03a37f4352bd2c00691fe851afae637e5d
ceridwenroberts <ceridwen.roberts@gmail.com>
2025-01-08 22:54:14 +0100

    add minor styling and  Context Provider to Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
+"use client";
+
+import { GameProvider } from "@/contexts/GameContext";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import GameBoard from "@/components/GameBoard";
+import ScoreBoard from "@/components/ScoreBoard";
+import cards from "@/data/cards.json";
+
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="m-auto">Main content
-        <GameBoard />
-      </main>
+      <GameProvider cardsData={cards}>
+        <main className="mx-auto w-full h-full flex flex-col">
+          <GameBoard />
+          <ScoreBoard />
+        </main>
+      </GameProvider>
       <Footer />
     </>
   );
