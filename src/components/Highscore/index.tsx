a055bb9250bcdac91d4706ff8150fdbdcf488c5d
ceridwenroberts <ceridwen.roberts@gmail.com>
2025-01-09 23:30:18 +0100

    update form submit button. Tests passed

--- src/components/Highscore/index.tsx
+++ src/components/Highscore/index.tsx
@@ -73,10 +73,14 @@ const Highscore = ({
           className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow autofocus"
           data-testid="input"
         />
-        <NewGameButton newRound={newRound} />
+        <button
+          type="submit"
+          className="w-full rounded-md bg-orange-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-orange-700 focus:shadow-none active:bg-orange-700 hover:bg-orange-600 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+        >
+          Done
+        </button>
       </form>
     </div>
   );
 };
-
 export default Highscore;
