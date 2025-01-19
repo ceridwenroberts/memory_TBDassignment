3ba40c9282c32da1524687f23fa91b13049d252e
ceridwenroberts <ceridwen.roberts@gmail.com>
2025-01-20 00:38:52 +0100

    clean up logic, remove storageMagager

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import Footer from "../components/Footer";
 import cardsData from "@/data/cardsData.json";
 import { CardType } from "@/lib/types";
 import { cardShuffler } from "@/util/cardShuffler";
-import { storageManager } from "@/util/storageManager";
 
 export default function Home() {
   const [cards, setCards] = useState<CardType[]>([]);
@@ -22,149 +21,81 @@ export default function Home() {
   const [currentHighscore, setCurrentHighscore] = useState<number>(0);
 
   useEffect(() => {
-    console.log("flipped", flipped.length);
-  }, [flipped])
+    newCards();
+    getStoredHighscore();
+  }, []);
 
-  const updateNewHighscore = (moves: number) => {
-    console.log("updatNewHighscore has been called");
-    localStorage.setItem("highscore", JSON.stringify(moves));
-    setShowForm(false);
+  const newCards = (): void => {
+    const shuffledCards = cardShuffler(cardsData);
+    setCards(shuffledCards);
   };
 
-  const shuffledCards = cardShuffler(cardsData);
-
-  // let currentHighscore: number = 0;
-  let currentHighscoreName: string = "";
-  const getStoredHighscore = () => {
+  const getStoredHighscore = (): void => {
     const storedName = localStorage.getItem("name");
     if (storedName) setHighscoreName(storedName);
     const highscoreData = localStorage.getItem("highscore");
-    console.log("stored highscore", highscoreData);
     if (highscoreData) {
-       setCurrentHighscore(JSON.parse(highscoreData));
+      setCurrentHighscore(JSON.parse(highscoreData));
     }
   };
 
-  const handleMockCurrent = (): void => {
-    const mockName = "bullen";
-    const mockHighscore = JSON.stringify(68);
-    
-    localStorage.setItem("name", mockName);
-    localStorage.setItem("highscore", mockHighscore);
-  }
-
-  const handleMockHighscore = (): void => {
-    console.log("Mock highscore")
-    setPaired(["a", "g", "r", "e", "s", "s"]);
-    setMoves(18);
-    const mockName = "Elin"
-    localStorage.setItem("name", mockName)
-    setHighscoreName(mockName);
-
-  }
-  const handleMockSolvedOnly = (): void =>{
-    console.log("SolvedOnly")
-  }
-
-  const newRound = () => {
-    getStoredHighscore();
-    setCards(shuffledCards);
-    setPaired([]);
+  const newRound = (): void => {
     setMoves(0);
-    setFlipped([])
+    setFlipped([]);
+    setPaired([]);
+    newCards();
   };
 
-  //ON LOAD
-
-  useEffect(() => {
+  const updateNewHighscore = (moves: number, nameQuery: string) => {
+    setHighscore(moves);
+    localStorage.setItem("highscore", JSON.stringify(moves));
+    setHighscoreName(nameQuery);
+    setShowForm(false);
     newRound();
-  }, []);
-
-  const handleOnClick = (card: CardType, index: number): void => {
-    setMoves(moves + 1);
-
-    if (flipped.length === 2 || paired.includes(card.name)) return;
-    setFlipped((prev: number[]): number[] => [...prev, index]); //type assertion needed here?
   };
 
-  // EVALUATE GUESS
-  // useEffect(() => {
-  //   if (flipped.length === 2) {
-  //     // setMoves(moves + 1);
+  const handleOnClick = (card: CardType, index: number) => {
+    if (flipped.length === 2 || paired.includes(card.name)) return;
+    const moveCount = moves + 1;
+    setMoves(moveCount);
+    if (flipped.length === 0) {
+      setFlipped([index]);
+    } else {
+      setFlipped((prev: number[]): number[] => [...prev, index]);
 
-  //     const checkPair = () => {
-  //  const [firstIndex, secondIndex] = flipped;
-  //     if (cards[firstIndex].name === cards[secondIndex].name) {
-  //       setPaired((prev: string[]): string[] => [
-  //         ...prev,
-  //         cards[firstIndex].name,
-  //       ]);
-  //       setFlipped([])
-  //     }
-
-  //     }
-   
-  //     setTimeout(() => {
-  //       checkPair();
-  //     }, 1000);
-  //   }
-  // }, [flipped, paired, cards]);
-
-  useEffect(() => {
-  if (flipped.length === 2) {
-    const checkPair = () => {
-      const [firstIndex, secondIndex] = flipped;
-      if (cards[firstIndex].name === cards[secondIndex].name) {
-        setPaired((prev: string[]): string[] => [
-          ...prev,
-          cards[firstIndex].name,
-        ]);
-      }
       setTimeout(() => {
-        setFlipped([]); 
+        const [firstIndex, secondIndex] = flipped;
+
+        if (cards[firstIndex].name === card.name) {
+          paired ?
+            setPaired((prev: string[]): string[] => [
+              ...prev,
+              cards[firstIndex].name,
+              cards[index].name,
+            ])
+          : setPaired([cards[firstIndex].name, cards[index].name]);
+        }
+        setFlipped([]);
+        const moveCount = moves + 1;
+        setMoves(moveCount);
       }, 1000);
-    };
+    }
+  };
 
-    setTimeout(checkPair, 1000);
-  }
-}, [flipped, cards]);
-
-
-  // CHECK WIN
   useEffect(() => {
-    console.log(paired);
-    if (paired.length > 1 && paired.length === cards.length / 2) {
-      console.log("passed win test");
-      if (highscore > moves) {
-         console.log("new highscore");
-               setShowForm(true);
-      } else {
-        // setGameEnd(true);
-        console.log("gameEnd no Highscore");
+    if (paired.length === 12) {
+      if (moves < highscore) {
+        setShowForm(true);
       }
     }
   }, [paired]);
-
- useEffect(() => {
-    // Find all elements with the 'data-my-attribute' attribute
-    const elementsWithAttribute = document.querySelectorAll('[data-testid]');
-   let cardImages = 0;
-    // Filter and log conditionally based on the value of the attribute
-    elementsWithAttribute.forEach((element) => {
-      if (element.getAttribute('data-testid') === 'card-image') {
-        // console.log('Matching element:', element);
-        cardImages = cardImages + 1;
-     console.log("Card images", cardImages)
-      }
-    });
-  }, [flipped]); 
 
   return (
     <>
       <Header />
       <main className="mx-auto w-full h-full flex flex-col">
         <div
-          className={`bg-blue-400 container grid grid-cols-4 max-w-l place-items-center gap-1 sm:gap-2 lg:gap-5 m-auto p-2 sm:p-5 `}
+          className={`container grid grid-cols-4 max-w-xl place-items-center gap-1 sm:gap-2 lg:gap-5 m-auto p-2 sm:p-5 `}
         >
           {cards.map((card: CardType, index: number) => {
             const isPaired = paired.includes(card.name);
@@ -172,57 +103,58 @@ export default function Home() {
               <div
                 key={index}
                 onClick={() => handleOnClick(card, index)}
-                className={` w-full aspect-square border border-1 rounded-md bg-slate-100 border-black grid place-items-center ${disabled ? "cursor-not-allowed pointer-events-none" : ""} ${isPaired && "opacity-50"}`}
+                className={`w-full aspect-square border border-1 rounded-md bg-slate-100 border-black grid place-items-center ${disabled ? "cursor-not-allowed pointer-events-none" : ""} ${isPaired && "opacity-50 transition-opacity duration-500"}`}
                 data-testid="card"
               >
                 {flipped.includes(index) || paired.includes(card.name) ?
-                  <Image
-                    src={card.imgSrc}
-                    width={500}
-                    height={500}
-                    alt={card.name}
-                    data-testid="card-image"
-                  />
-                  :
-                  <div
-                    // className={`${flipped.length === 2 ? "cursor-not-allowed pointer-events-none" : ""}`}
-                  >
-                    <p>?</p>
+                  <div data-testid="card-image">
+                    <Image
+                      src={card.imgSrc}
+                      width={500}
+                      height={500}
+                      alt={card.name}
+                    />
+                  </div>
+                : <div>
+                    <p className="text-4xl sm:text-5xl md:text-7xl ">?</p>
                   </div>
                 }
               </div>
             );
           })}
         </div>
-        {/* Debuttons */ }
-        <div className="inline-flex justify-around p-5 *:rounded-full *:border *:border-sky-100 *:bg-sky-50 *:px-2 *:py-0.5">
-        <button onClick={ () => setShowForm(true) }>Show form</button>
-        {/* <button onClick={ handleMockCurrent } >Mock Current</button>
-        <button onClick={ handleMockHighscore }>Highscore win</button>
-        <button onClick={ handleMockSolvedOnly }>Solved only</button> */}
-        </div>
-        {/* Scoreboard */}
         <div className="flex justify-around w-full py-6 px-4 text-sm leading-6">
           <div className="flex flex-col place-items-center">
             <p>Moves</p>
             <p data-testid="moves">{moves}</p>
           </div>
-          <NewGameButton newRound={newRound} data-testid="new-game-btn" />
+          <NewGameButton
+            data-testid="new-game-btn"
+            newRound={newRound}
+            paired={paired}
+          />
           <div className="flex flex-col place-items-center">
             <p>Highscore</p>
-            
+
             <div className="inline-flex gap-1">
-              {highscoreName && (
-           <>    <p data-testid="highscore-name">{ `${highscoreName}:` }</p>
-                  <p data-testid="highscore">{ highscore }
-                  </p></>
-              )}
+              {highscoreName ?
+                <>
+                  {" "}
+                  <p data-testid="highscore-name">{`${highscoreName}:`}</p>
+                  <p data-testid="highscore">{highscore}</p>
+                </>
+              : <>
+                  <p>0</p>
+                </>
+              }
             </div>
           </div>
         </div>
         {showForm && (
           <Highscore
-            updateNewHighscore={() => updateNewHighscore(moves)}
+            updateNewHighscore={(nameQuery) =>
+              updateNewHighscore(moves, nameQuery)
+            }
           />
         )}
       </main>
