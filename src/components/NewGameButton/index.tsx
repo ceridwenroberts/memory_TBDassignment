94b8de34902b81b807202f19b13d3c4d679b417d
ceridwenroberts <ceridwen.roberts@gmail.com>
2025-01-20 00:35:16 +0100

    add newRound trigger to New Game button

--- src/components/NewGameButton/index.tsx
+++ src/components/NewGameButton/index.tsx
@@ -1,14 +1,18 @@
-type NewGameButtonProps = {};
+import { MouseEvent } from "react";
 
-const NewGameButton = ({ }: NewGameButtonProps) => {
-    
-  // For conditional styling
-  //     ${gameStarted ? "bg-orange-800 bg-opacity-25 text-opacity-100" : "" // Set to inactive styling before first move
-  //     }
-  //   ${gameEnd && "bg-orange-800 text-orange-50 "} //Highlight after game complete
+type NewGameButtonProps = {
+  newRound: () => void;
+};
+
+const NewGameButton = ({ newRound }: NewGameButtonProps) => {
+  const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    newRound();
+  };
 
   return (
     <button
+      onClick={handleClick}
       className={`border border-orange-800 rounded-md py-2 px-4 text-center text-orange-800 text-sm hover:shadow-lg`}
       data-testid="new-game-btn"
     >
